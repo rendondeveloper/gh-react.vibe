df60cbe337164f47c6427265cb9e8aab225aa463
Luis Enrique Rendon Cortez <luis.rendon@justo.mx>
2022-10-12 09:21:24 -0500

    fix bug 3

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base:'https://rendondeveloper.github.io/gh-react.vite',
+  base: 'https://rendondeveloper.github.io/gh-react.vibe/',
   plugins: [react()]
 })
